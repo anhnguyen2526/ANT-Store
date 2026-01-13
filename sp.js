277ffeaa6891cfa8260de432423ab677fa34b68c
anhnguyen2526 <anhnguyen778@gmail.com>
2026-01-13 12:27:41 +0700

    Add files via upload

--- sp.js
+++ sp.js
@@ -21,3 +21,33 @@ window.addEventListener("load", () => {
     }, 600);
   }, remaining);
 });
+
+// Lấy phần tử logo và ảnh bên trong
+const logo = document.querySelector('.floating-logo'); 
+const logoImg = logo.querySelector('img');
+
+// Khi click vào logo
+logo.addEventListener('click', () => {
+  lightbox.style.display = 'flex';
+  lightboxImg.src = logoImg.src; // ảnh hiện tại của logo
+});
+
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeBtn = document.querySelector('.close');
+
+logo.addEventListener('click', () => {
+  lightbox.style.display = 'flex';
+  lightboxImg.src = logoImg.src;
+});
+
+closeBtn.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
+
+// Click ngoài ảnh cũng đóng
+lightbox.addEventListener('click', (e) => {
+  if (e.target === lightbox) {
+    lightbox.style.display = 'none';
+  }
+});
