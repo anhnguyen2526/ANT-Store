ee8b67f17ebc952c563d97287e77d90c4b8725ba
anhnguyen2526 <anhnguyen778@gmail.com>
2026-01-13 16:03:52 +0700

    Add files via upload

--- sp.js
+++ sp.js
@@ -1,25 +1,23 @@
 const startTime = Date.now();
 
 window.addEventListener("load", () => {
-  const loader = document.getElementById("loader");
-  const content = document.getElementById("page-content");
+  const loader = document.getElementById("loader");        // loader
+  const content = document.getElementById("page-content"); // nội dung trang
 
   const elapsed = Date.now() - startTime;
-  const minTime = 6000; // 6 giây
+  const minTime = 4900; // 6 giây
 
   const remaining = Math.max(minTime - elapsed, 0);
 
   setTimeout(() => {
+    // Ẩn loader
+    loader.classList.add("hide");
     // Hiện nội dung
     content.style.display = "block";
 
-    // Ẩn loader
-    loader.classList.add("hide");
-
+    // Sau 0.6s xóa hoàn toàn loader khỏi DOM
     setTimeout(() => {
       loader.remove();
     }, 600);
   }, remaining);
 });
-
-
